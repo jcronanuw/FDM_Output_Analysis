6f99d657c7c38790b733b9fc8b3cfe6d63afdcea
jcronan@uw.edu <jcronan@uw.edu>
2023-02-25 22:13:44 -0500

    Updated fine fuels graphics script to produce set of line graphs (data every 5 years) and box plots (data every 10 years). Script produces a boxplot figure ready for publication. Not sure about line graph. Decided not to use them in this chapter. More for reference at this point. Also started writing script to produce graphics for mean fire interval (mFRI). This script produces a draft version of the line graph that still needs to be refined. Have not done any work on the boxplot() section.

--- 20230131_FDM_Derivative_Graphics_Mean_Fire_Interval.R
+++ 20230131_FDM_Derivative_Graphics_Mean_Fire_Interval.R
@@ -1,7 +1,7 @@
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#PURPOSE: This script generates graphics from tabular data for fine fuels tables produced
-#in 20230131_FDM_Derivative_Processing_Fine_Fuels.R scrip in this GitHub repository.
+#PURPOSE: This script generates graphics from tabular data for mean fire interval tables produced
+#in 20230131_FDM_Derivative_Processing_mFRI.R scrip in this GitHub repository.
 #GitHub Repository: FDM_Output_Analysis
 #Graphics:
 #1) Line graphs
@@ -13,7 +13,7 @@
 #400 N 34th Street
 #Suite 201
 #Seattle, WA 98103
-#Date Created: 1-Feb-2023
+#Date Created: 25-Feb-2023
 
 library(fields)#for set.panel()
 library(ggplot2)#for boxplots
@@ -34,12 +34,12 @@ usfs <- "C:/Users/jcronan/OneDrive - USDA/"
 setwd(paste(usfs, "Documents/FDM_2023_Simulation_Data/Step_05_Derivative_Tables", sep = ""))
 
 #Import input parameters
-dt_csv <- read.csv("Derivative_table_fine_fuels.csv", header=TRUE, 
+dt_csv <- read.csv("Derivative_table_mfri.csv", header=TRUE, 
                    sep=",", na.strings="NA", dec=".", strip.white=TRUE)
 
 #Look at totals for each row. This is the number of pixels in each map. These numbers should
 #all be the same, or at least in a very narrow range.
-dt_sum <- apply(dt_csv[,4:24],1,sum)
+dt_sum <- apply(dt_csv[,4:10],1,sum)
 range(dt_sum)
 plot(dt_sum)
 total_pixels <- mean(dt_sum)
@@ -60,29 +60,20 @@ total_area <- 2100256
 #Common name for table showing pixels per category
 dtp <- dt_csv
 
-dtp_0_2 <- dtp$X1 + dtp$X2
-dtp_2_4 <- dtp$X3 + dtp$X4
-dtp_4_6 <- dtp$X5 + dtp$X6
-dtp_6_10 <- dtp$X7 + dtp$X8 + dtp$X9 + dtp$X10
-dtp_11_ <- dtp$X11 + dtp$X12 + dtp$X13 + dtp$X14 + dtp$X15 + dtp$X16 + dtp$X17 + dtp$X18 + dtp$X19 + dtp$X20 + dtp$X21
-dtp_4_ <- dtp$X5 + dtp$X6 + dtp$X7 + dtp$X8 + dtp$X9 + dtp$X10 + dtp$X11 + dtp$X12 + dtp$X13 + dtp$X14 + dtp$X15 + dtp$X16 + dtp$X17 + dtp$X18 + dtp$X19 + dtp$X20 + dtp$X21
-
-dtp <- data.frame(dtp[,1:3], L_0_2 = dtp_0_2, L_2_4 = dtp_2_4, L_4_6 = dtp_4_6, L_6_10 = dtp_6_10, L_10_ = dtp_11_, L_4_ = dtp_4_)
-
+dtp <- data.frame(dtp[,1:3], dtp[,5:9])
 
 #Convert pixels to percent of EAFB
-#dtr <- t(apply(dtp[1:length(dtp[,1]),4:24], 1, function(x) round((x/total_area)*100,1)))
-dtr <- t(apply(dtp[1:length(dtp[,1]),4:9], 1, function(x) round((x/total_area)*100,1)))
+dtr <- t(apply(dtp[1:length(dtp[,1]),4:8], 1, function(x) round((x/total_area)*100,1)))
 
 dt <- data.frame(dtp[,1:3], dtr)
 
 #Generate a 3 - panel plot of fine fuel loading over time for low, medium, and high weight surface fuels
 dev.off()
-set.panel(3,1)
+set.panel(2,2)
 cf <- 0.5
 
 #Total fine fuel loading < 4.5 Mg/ha
-i <- 4
+i <- 5
 scenario <- c(50,75,100,125)
 ct <- as.character(c("blue", "green", "orange", "pink"))
 par(tcl=-0.5, family="serif", mai=c(0.3,0.6,0.3,0.3))
@@ -98,7 +89,7 @@ for(a in 1:length(scenario))
     {
     if(b == 1 & scenario[a] == 50)
       {
-      plot(dt$sim_yr[min_ss[b]:max_ss[b]], dt[min_ss[b]:max_ss[b],i], type = "l", col = lc, ylim = c(30,55),
+      plot(dt$sim_yr[min_ss[b]:max_ss[b]], dt[min_ss[b]:max_ss[b],i], type = "l", col = lc, ylim = c(0,100),
            ylab = "")
       } else
         {
@@ -107,13 +98,13 @@ for(a in 1:length(scenario))
   }
 }
 lines(dt$sim_yr[min_ss[1]:max_ss[1]], rep(dt[min_ss[1],i],11), col = "black", lty = 2, lwd = 2)
-legend(3,42, c("Baseline", "50k/yr", "75k/yr", "100k/yr", "125k/yr"), col = c("black", "blue", "green", "orange", "pink"), 
+legend(3,90, c("Baseline", "50k/yr", "75k/yr", "100k/yr", "125k/yr"), col = c("black", "blue", "green", "orange", "pink"), 
        lty = c(2,1,1,1,1))
-text(0, 53, "A)")
+text(0, 95, "A)")
 #title("Fine Fuel Loading Less Than 4.5 Mg/ha")
 
 #Total fine fuel loading 4.5 - 9 Mg/ha
-i <- 5
+i <- 6
 scenario <- c(50,75,100,125)
 ct <- as.character(c("blue", "green", "orange", "pink"))
 par(tcl=-0.5, family="serif", mai=c(0.3,0.6,0.3,0.3))
@@ -129,7 +120,7 @@ for(a in 1:length(scenario))
   {
     if(b == 1 & scenario[a] == 50)
     {
-      plot(dt$sim_yr[min_ss[b]:max_ss[b]], dt[min_ss[b]:max_ss[b],i], type = "l", col = lc, ylim = c(10,35), 
+      plot(dt$sim_yr[min_ss[b]:max_ss[b]], dt[min_ss[b]:max_ss[b],i], type = "l", col = lc, ylim = c(0,100), 
            ylab = "Percent of Eglin Air Force Base's Land Area")
     } else
     {
@@ -138,11 +129,11 @@ for(a in 1:length(scenario))
   }
 }
 lines(dt$sim_yr[min_ss[1]:max_ss[1]], rep(dt[min_ss[1],i],11), col = "black", lty = 2, lwd = 2)
-text(0, 33, "B)")
+text(0, 95, "B)")
 #title("Fine Fuel Loading 4.5-9.0 Mg/ha")
 
 #Total fine fuel loading > 9 Mg/ha
-i <- 9
+i <- 7
 scenario <- c(50,75,100,125)
 ct <- as.character(c("blue", "green", "orange", "pink"))
 par(tcl=-0.5, family="serif", mai=c(0.6,0.6,0.3,0.3))
@@ -158,7 +149,7 @@ for(a in 1:length(scenario))
   {
     if(b == 1 & scenario[a] == 50)
     {
-      plot(dt$sim_yr[min_ss[b]:max_ss[b]], dt[min_ss[b]:max_ss[b],i], type = "l", col = lc, ylim = c(15,45), 
+      plot(dt$sim_yr[min_ss[b]:max_ss[b]], dt[min_ss[b]:max_ss[b],i], type = "l", col = lc, ylim = c(0,100), 
            xlab = "Simulation Year", ylab = "")
     } else
     {
@@ -167,8 +158,38 @@ for(a in 1:length(scenario))
   }
 }
 lines(dt$sim_yr[min_ss[1]:max_ss[1]], rep(dt[min_ss[1],i],11), col = "black", lty = 2, lwd = 2)
-text(0, 43, "C)")
+text(0, 95, "C)")
 #title("Fine Fuel Loading Greater Than 9.0 Mg/ha")
+
+#Total fine fuel loading > 9 Mg/ha
+i <- 8
+scenario <- c(50,75,100,125)
+ct <- as.character(c("blue", "green", "orange", "pink"))
+par(tcl=-0.5, family="serif", mai=c(0.6,0.6,0.3,0.3))
+
+for(a in 1:length(scenario))
+{
+  ss <- which(dt$rx_fire == scenario[a])
+  lc <- ct[a]
+  min_ss <- seq(min(ss),max(ss),11)
+  max_ss <- seq((min(ss)+10),max(ss),11)
+  runs <- sort(unique(dt$run_no))
+  for(b in 1:length(runs))
+  {
+    if(b == 1 & scenario[a] == 50)
+    {
+      plot(dt$sim_yr[min_ss[b]:max_ss[b]], dt[min_ss[b]:max_ss[b],i], type = "l", col = lc, ylim = c(0,100), 
+           xlab = "Simulation Year", ylab = "")
+    } else
+    {
+      lines(dt$sim_yr[min_ss[b]:max_ss[b]], dt[min_ss[b]:max_ss[b],i], col = lc)
+    }
+  }
+}
+lines(dt$sim_yr[min_ss[1]:max_ss[1]], rep(dt[min_ss[1],i],11), col = "black", lty = 2, lwd = 2)
+text(0, 95, "D)")
+#title("Fine Fuel Loading Greater Than 9.0 Mg/ha")
+
 
 ###############################################################################################################
 ###############################################################################################################
